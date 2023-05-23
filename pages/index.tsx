fd1ca29b054509c189d98df7a0932ac805d021a6
Elijah2609 <elijahlyndon14@gmail.com>
2023-05-23 20:37:13 +0100

    redesign

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { Container, IntroContainer, Bio, ImageContainer, WarpedImage, Socials, TechStack } from '../styles/index-styles';
 import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 import { AiOutlineHtml5 } from 'react-icons/ai';
 import { DiCss3 } from 'react-icons/di';
 import { FaReact, FaNodeJs } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io5';
 import { SiStyledcomponents, SiNextdotjs, SiTypescript } from 'react-icons/si';
+import { Bio, Container, ImageContainer, IntroContainer, Socials, TechIcon, TechStackContainer, WarpedImage } from '../styles/index-styles';
 
+
+const TechStack = () => {
+  return (
+    <TechStackContainer>
+      <h3>Tech Stack</h3>
+      <TechIcon>
+        <AiOutlineHtml5 style={{ color: '#E34F26' }} />
+      </TechIcon>
+      <TechIcon>
+        <DiCss3 style={{ color: '#1572B6' }} />
+      </TechIcon>
+      <TechIcon>
+        <IoLogoJavascript style={{ color: '#dbc51f' }} />
+      </TechIcon>
+      <TechIcon>
+        <SiTypescript style={{ color: '#007ACC' }} />
+      </TechIcon>
+      <TechIcon>
+        <FaReact style={{ color: '#61DAFB' }} />
+      </TechIcon>
+      <TechIcon>
+        <SiStyledcomponents style={{ color: '#DB7093' }} />
+      </TechIcon>
+      <TechIcon>
+        <FaNodeJs style={{ color: '#43853D' }} />
+      </TechIcon>
+      <TechIcon>
+        <SiNextdotjs style={{ color: '#000000' }} />
+      </TechIcon>
+    </TechStackContainer>
+  );
+};
 
 const Home = () => {
   return (
@@ -17,32 +49,20 @@ const Home = () => {
           <h3>
             Hi, I'm Elijah McNamara, welcome to my website! I'm a passionate front-end engineer with a love for code. Take a look at some of my projects 👇
           </h3>
-          <Socials>
-            <a href="https://github.com/Elijah10000">
-              <FaGithubSquare className="fa" style={{ fontSize: '36px' }} />
-            </a>
-            <a href="https://linkedin.com/in/elijah-mcnamara">
-              <FaLinkedin className="fa" style={{ fontSize: '36px' }} />
-            </a>
-          </Socials>
         </IntroContainer>
         <ImageContainer>
-          <WarpedImage src="/assets/Picture.jpg" alt="Picture" style={{ height: "250px", width: "250px", borderRadius: "90px" }} />
+          <WarpedImage src="/assets/Picture.jpg" alt="Picture" style={{ height: '250px', width: '250px', borderRadius: '90px' }} />
         </ImageContainer>
+        <Socials>
+          <a href="https://github.com/Elijah10000">
+            <FaGithubSquare className="fa" style={{ fontSize: '40px' }} />
+          </a>
+          <a href="https://linkedin.com/in/elijah-mcnamara">
+            <FaLinkedin className="fa" style={{ fontSize: '40px' }} />
+          </a>
+        </Socials>
       </Bio>
-      <TechStack>
-          <div style={{ display: 'flex', gap: '30px' }}>
-          <h3>Tech Stack</h3>
-            <AiOutlineHtml5 className="tech" style={{ fontSize: '60px', color: '#E34F26' }} />
-            <DiCss3 className="tech" style={{ fontSize: '60px', color: '#1572B6' }} />
-            <IoLogoJavascript className="tech" style={{ fontSize: '60px', color: '#dbc51f' }} />
-            <SiTypescript className="tech" style={{ fontSize: '60px', color: '#007ACC' }} />
-            <FaReact className="tech" style={{ fontSize: '60px', color: '#61DAFB' }} />
-            <SiStyledcomponents className="tech" style={{ fontSize: '60px', color: '#DB7093' }} />
-            <FaNodeJs className="tech" style={{ fontSize: '60px', color: '#43853D' }} />
-            <SiNextdotjs className="tech" style={{ fontSize: '60px', color: '#000000' }} />
-          </div>
-        </TechStack>
+      <TechStack />
     </Container>
   );
 };
--- styles/index-styles.tsx
+++ styles/index-styles.tsx
@@ -1,25 +1,24 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   height: 100vh;
 `;
 
 export const Bio = styled.div`
-  width: 65%;
-  height: 60%;
-  margin-top: 12%;
-  margin-left: 18%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2rem;
+  padding: 2rem;
 `;
 
 export const IntroContainer = styled.div`
-  width: 40%;
-  height: 47%;
-  display: flex;
-  flex-direction: column;
-  margin-top: 2%;
+  text-align: center;
 
   h1 {
     font-size: 2.5rem;
@@ -27,63 +26,65 @@ export const IntroContainer = styled.div`
     color: #2d2e32;
     line-height: 1.2;
     margin-bottom: 1rem;
-    margin-top: 2rem;
-    transition: color 0.5s ease, transform 0.5s ease;
     font-family: Poppins, sans-serif;
-
+    transition: transform 0.5s ease;
+  
     &:hover {
       transform: scale(1.07);
     }
   }
-
+  
   h3 {
     font-size: 1.2rem;
     font-weight: 300;
     color: #757272;
     font-family: Poppins, sans-serif;
     margin-top: 0.5rem;
-    transition: color 0.5s ease, transform 0.5s ease;
+    transition: transform 0.5s ease;
+  
+  &:hover {
+    transform: scale(1.07);
+    }
   }
 `;
 
 export const ImageContainer = styled.div`
-  margin-left: 5%;
-  margin-top: 4%;
-  height: 52%;
-`;
-
-export const warpAnimation = keyframes`
-  0% {
-      border-radius: 60% 40% 30% 70%/60% 30% 70% 40%;
-  }
-  50% {
-      border-radius: 30% 60% 70% 40%/50% 60% 30% 60%;
-  }
-  100% {
-      border-radius: 60% 40% 30% 70%/60% 30% 70% 40%;
-  }
+  margin-top: 2rem;
+  margin-bottom: 1rem;
 `;
 
 export const WarpedImage = styled.img`
-  border: 3px solid black;  
-  animation: ${warpAnimation} 3s infinite;
-  transition: color 0.5s ease, transform 0.5s ease;
-  &:hover {
+  border: 3px solid black;
+  animation: warpAnimation 3s infinite;
+  transition: transform 0.5s ease;
+
+    &:hover {
       transform: scale(1.07);
     }
+
+  @keyframes warpAnimation {
+    0% {
+      border-radius: 60% 40% 30% 70%/60% 30% 70% 40%;
+    }
+    50% {
+      border-radius: 30% 60% 70% 40%/50% 60% 30% 60%;
+    }
+    100% {
+      border-radius: 60% 40% 30% 70%/60% 30% 70% 40%;
+    }
+    
+  }
 `;
 
 export const Socials = styled.div`
-  margin-top: 4%;
   display: flex;
-  height: 15%;
-  width: 33%;
+  justify-content: center;
+  margin-top: 1rem;
+  transition: transform 0.5s ease;
 
   a {
     margin-right: 0.5rem;
     color: #2d2e32;
-    transition: color 0.5s ease, transform 0.5s ease;
-    margin-bottom: 1rem;
     font-size: 1.5rem;
 
     &:hover {
@@ -92,16 +93,24 @@ export const Socials = styled.div`
   }
 `;
 
-export const TechStack = styled.div`
-  justify-content: flex-end;
-  height: 10%;
-  background-color: red;
-  
+export const TechStackContainer = styled.div`
   h3 {
-    border-right: 2px solid rgba(45, 46, 50, 0.5);
-    width: 30%;
-    color: #2d2e32;
-    font-family: Mulish, sans-serif;
-    font-weight: 600;
+    border-right: 2px solid rgba(45,46,50,.5);
+    padding-right: 1.5rem;  
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 25px;
+`;
+
+export const TechIcon = styled.div`
+  font-size: 50px;
+  transition: transform 0.3s ease;
+
+  &:hover {
+    transform: scale(1.20);
   }
 `;
